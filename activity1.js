cee07b71f5ce485cf30c4908e040fc1b9a9acb0a
krish703 <78411788+krish703@users.noreply.github.com>
2021-06-03 11:35:12 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,24 @@
+score=0;
+
+function updateScore(){
+    score=score+1;
+    document.getElementById("score").innerHTML="score: "+ score;
+}
+
+
+function saveScore(){
+localStorage.setItem ("score",score);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
